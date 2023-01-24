8ca06cfb82c9310fd97ccc4d49e61cedd03be535
narvalis <66041153+fevenegas@users.noreply.github.com>
2023-01-24 00:00:48 -0300

    template del nivel demo

---
+++ Dog in the Streets.v1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    /*// Start is called before the first frame update
+    void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
