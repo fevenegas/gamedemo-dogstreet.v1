bc28a6b99a833cff313c603a349251a7fe8ad832
narvalis <66041153+fevenegas@users.noreply.github.com>
2023-02-27 22:38:12 -0300

    Rework - movement, basic collide & camera

---
+++ Dog in the Streets.v1/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+    private void LateUpdate() {
+        Vector3 delta = Vector3.zero;
+
+        // Checking if it's inside the bounds in the X axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX) {
+            if (transform.position.x < lookAt.position.x) // if the middle of the camera is smaller than the focus position
+                delta.x = deltaX - boundX;
+            else
+                delta.x = deltaX + boundX;
+        }
+
+        // Checking if it's inside the bounds in the Y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY) {
+            if (transform.position.y < lookAt.position.y) // if the middle of the camera is smaller than the focus position
+                delta.y = deltaY - boundY;
+            else
+                delta.y = deltaY + boundY;
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
--- Dog in the Streets.v1/Assets/Scripts/PlayerMovement.cs
+++ Dog in the Streets.v1/Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(BoxCollider2D))]
+
 public class PlayerMovement : MonoBehaviour {
 
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
-    /*// Start is called before the first frame update
-    void Start()
-    {
+    private void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate() {
         
-    }*/
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction, wether you're going left or right
+        if(moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if(moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Collide: casts a box in the direction to check if it can move in the "y" axis
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y),
+                Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        
+        if (hit.collider == null) {
+            // Movement
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        // Collide: casts a box in the direction to check if it can move in the "x" axis
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0),
+                Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        
+        if (hit.collider == null) {
+            // Movement
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
         
     }
 }
